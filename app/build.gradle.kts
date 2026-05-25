de77a89525983183d042fe845158934ad4c4e5d0
wiskas-sys <wsinduadi@gmail.com>
2026-05-26 05:35:10 +0700

    +api

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,10 +44,18 @@ android {
         implementation("com.github.bumptech.glide:glide:4.16.0")
         implementation("androidx.viewpager2:viewpager2:1.0.0")
         implementation("com.tbuonomo:dotsindicator:5.0")
-
-        // Retrofit
+        implementation("androidx.core:core-ktx:1.12.0")
+        implementation("androidx.appcompat:appcompat:1.6.1")
+        implementation("com.google.android.material:material:1.11.0")
+        implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+// Retrofit
         implementation("com.squareup.retrofit2:retrofit:2.9.0")
         implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+        implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+// Glide
+        implementation("com.github.bumptech.glide:glide:4.16.0")
+// CardView
+        implementation("androidx.cardview:cardview:1.0.0")
 
         // OkHttp
         implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
